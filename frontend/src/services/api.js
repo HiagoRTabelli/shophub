6f88ad9effdb6d3e0575badc7839f8588360e199
Hiago Rodrigues <hiagort7@gmail.com>
2026-06-16 18:15:15 -0300

    Configure frontend API for production backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://shophub-backend-3cgm.onrender.com/api",
 });
 
 export default api;
